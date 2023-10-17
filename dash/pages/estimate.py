35120d449df0f3b374af449ceb5280448a1e5466
moiseberthe <berthemoise890@gmail.com>
2023-10-17 16:28:07 +0200

    first regressor model & new form on estimate page

--- dash/pages/estimate.py
+++ dash/pages/estimate.py
@@ -7,26 +7,106 @@ fields = [
     {'type': 'number', 'label' : 'Nombre de lot', 'id': 'nb-lots'},
     {'type': 'number', 'label' : 'Nombre de pièces principales', 'id': 'nb-pieces'}
 ]
+fields = [
+    {'id': 'select-departement'},
+    {'id': 'select-type-local'},
+    {'id': 'surfaces'},
+    {'id': 'nb-pieces'},
+]
 
 dash.register_page(__name__)
-form = dbc.Row(
-    [
-        dbc.Col(
-            dbc.FormFloating([
-                dbc.Input(type=f['type'], id=f['id'], placeholder=f['label']),
-                dbc.Label(f['label']),
-            ]),
-            width=6,
-        ) for f in fields
-    ],
-    className='mb-3',
-)
+# form = dbc.Row(
+#     [
+#         dbc.Col(
+#             dbc.FormFloating([
+#                 dbc.Input(type=f['type'], id=f['id'], placeholder=f['label']),
+#                 dbc.Label(f['label']),
+#             ]),
+#             width=6,
+#         ) for f in fields
+#     ],
+#     className='mb-3',
+# )
 
 layout = html.Div([
-    form,
-    dbc.Button('Primary', color='primary', id='submit-val', n_clicks=0),
+    html.H3('Localisation', className='mb-3'),
+    dbc.Row([
+        dbc.Col(
+            dbc.FormFloating([
+                dbc.Select(
+                    id="select-region",
+                    options=[
+                        {"label": "Option 0", "value": "0"},
+                        {"label": "Option 1", "value": "1"},
+                        {"label": "Option 2", "value": "2"},
+                    ],
+                    value='0'
+                ),
+                dbc.Label(['Région']),
+            ]),
+            width=6
+        ),
+        dbc.Col(
+            dbc.FormFloating([
+                dbc.Select(
+                    id="select-departement",
+                    options=[{"label": "Option 0", "value": "0"},],
+                    value='0'
+                ),
+                dbc.Label(['Département']),
+            ]),
+            width=6
+        ),
+    ], className='mb-5'),
+    html.H3('Caractéristiques du bien', className='mb-3'),
+    dbc.Row([
+        dbc.Col(
+            dbc.FormFloating([
+                dbc.Select(
+                    id="select-type-local",
+                    options=[
+                        {"label": "Option 0", "value": '12', 'selected': True},
+                        {"label": "Option 1", "value": "1"},
+                        {"label": "Option 2", "value": "2"},
+                    ],
+                    value='12',
+                ),
+                dbc.Label(['Type']),
+            ]),
+            width=4
+        ),
+        dbc.Col(
+            dbc.FormFloating([
+                dbc.Input(type='number', id='surface', placeholder='surface'),
+                dbc.Label('Surface'),
+            ]),
+            width=4
+        ),
+        dbc.Col(
+            dbc.FormFloating([
+                dbc.Input(type='number', id='nb-pieces', placeholder='surface'),
+                dbc.Label('Nombre de pièces principales'),
+            ]),
+            width=4
+        ),
+    ], className='mb-4'),
+    dbc.Button('Estimer', color='primary', id='submit-val', n_clicks=0),
     html.Div(id='container-button-basic')
 ], className='container')
+
+@callback(
+    Output('select-departement', 'options'),
+    Input('select-region', 'value')
+)
+def update_depts(value):
+    if(not value):
+        return []
+    return [
+        {"label": "Option 0", "value": "1", "selected": 'selected'},
+        {"label": "Option 1", "value": "1"},
+        {"label": "Option 2", "value": "2"},
+        {"label": "Disabled option", "value": "3", "disabled": True}
+    ]
 
 @callback(
     Output('container-button-basic', 'children'),
