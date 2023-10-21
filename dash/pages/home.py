dce61832e19597f98526ece7bdd657f9dacb58cd
moiseberthe <berthemoise890@gmail.com>
2023-10-21 22:32:48 +0200

    Regression models

--- dash/pages/home.py
+++ dash/pages/home.py
@@ -1,34 +1,16 @@
-
 import pandas as pd
 import plotly.express as px
 import dash
 from dash import Dash, html, dcc, callback, Output, Input
 import json
 
-years = [18, 19, 20, 21]
-frames = []
-for year in years:
-    frames.append(
-        pd.read_csv(f'../assets/data/valeursfoncieres-20{year}.txt', sep='|', decimal=',', nrows=10000, low_memory=False)
-    )
-df = pd.concat(frames)
-dfC = df.copy()
-df.shape
-
-dfAddress = pd.read_csv('../assets/data/communes-departement-region.csv', sep=',')
-dfAddress = dfAddress.drop_duplicates(subset=['code_departement'])
-
-dfRegions = pd.DataFrame(dfAddress.code_region.ffill().unique(), columns=['code_region'])
-dfRegions.code_region = dfRegions.code_region.astype('int').astype('str').str.pad(2, side='left', fillchar='0')
-
-
-dfDepts = pd.DataFrame(dfAddress.code_departement.ffill().unique(), columns=['code_departement'])
-dfDepts.code_departement = dfDepts.code_departement.astype('str').str.pad(2, side='left', fillchar='0')
+df = pd.read_csv(f'../assets/data/dashboard-data.csv', sep='|', nrows=40000, low_memory=False)
+dfRegions = pd.read_csv('../assets/data/regions.csv')
+dfDepts = pd.read_csv('../assets/data/departement.csv')
+df.code_departement = df.code_departement.astype('int').astype('str').str.pad(2, side='left', fillchar='0')
 
 dfScales = {'region': dfRegions, 'departement': dfDepts}
-
 geojson = {}
-
 
 with open('../assets/data/metropole-version-simplifiee.geojson') as response:
     geojson['metropole'] = json.load(response)
@@ -39,51 +21,9 @@ with open('../assets/data/regions-version-simplifiee.geojson') as response:
 with open('../assets/data/departements-version-simplifiee.geojson') as response:
     geojson['departement'] = json.load(response)
 
-df['key'] = df['Date mutation'].astype('str') + df['Type de voie'].astype('str') + df['Voie'].astype('str') + df['Code postal'].astype('str')
-df = df.groupby('key').filter(lambda x : len(x) == 1)
-
-
-df = df[df['Valeur fonciere'] > 1]
-df = df[df['Type local'].notna()]
-
-missingValueRate = df.isna().mean() * 100
-df = df[df.columns[missingValueRate < 65]]
-
-df.drop(['No disposition', 'No plan', 'Section', 'Nature culture'], axis=1, inplace=True)
-
-df['Date mutation'] = pd.to_datetime(df['Date mutation'], format="%d/%m/%Y")
-df['annee'] = df['Date mutation'].dt.year
-df['mois'] = df['Date mutation'].dt.month
-
-df['Code departement'] = df['Code departement'].astype('str').str.pad(2, side='left', fillchar='0')
-dfAddress['code_departement'] = dfAddress['code_departement'].astype('str').str.pad(2, side='left', fillchar='0')
-
-df = df.merge(dfAddress.loc[:, ['code_departement', 'code_region', 'nom_region', 'latitude', 'longitude']], how='left', left_on='Code departement', right_on='code_departement')
-
-df['code_region'] = df['code_region'].astype('int').astype('str').str.pad(2, side='left', fillchar='0')
-
 echelle = {
     'Departement': 'departement',
     'Region' : 'region',
-}
-
-filterStyle = {
-    'display': 'flex',
-    'alignItems': 'center',
-    'gap': '8px'
-}
-dropdownStyle = {
-    'width': '200px'
-}
-cardStyle = {
-    'padding': '10px',
-    'display': 'flex',
-    'borderRadius': '8px',
-    'textAlign': 'center',
-    'width': '30%',
-    'boxShadow': 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',
-    'flexDirection': 'column',
-    'gap': '8px',
 }
 
 def inputTolist(value):
@@ -91,6 +31,12 @@ def inputTolist(value):
         return [int(value)]
     else:
         return [int(x) for x in value]
+    
+def card(title, body=[], id=''):
+    return html.Div([
+            html.H3(children=title, className='cs-card-title'),
+            html.Label(id=id, className='cs-card-body')
+        ], className='cs-card'),
 
 dash.register_page(__name__, path='/')
 
@@ -98,64 +44,93 @@ layout = html.Div([
     html.Div([
         html.Div([
             html.Label(children='Année'),
-            dcc.Dropdown(df['annee'].unique(), '2018', id='years-selection', style=dropdownStyle, multi=True),
-        ], style=filterStyle),
+            dcc.Dropdown(df['annee'].unique(), '2018', id='years-selection', className='select-field', multi=True),
+        ], className='filter-name'),
 
         html.Div([
             html.Label(children='Echelle'),
-            dcc.Dropdown(list(echelle.keys()), 'Departement', id='scale-selection', style={'width': '150px'}),
-        ], style=filterStyle),
-    ], style={'display':'flex', 'gap': '16px'}),
+            dcc.Dropdown(list(echelle.keys()), 'Departement', id='scale-selection', className='select-field'),
+        ], className='filter-name'),
+    ], style={'display':'flex', 'gap': '16px', 'justifyContent': 'end'}),
 
     html.Div([
+        html.H3(children=[
+            html.Span('Chiffres des ventes '), 
+            html.Span(id='filter-dates-name'), 
+            html.Span(' par '), 
+            html.Span(id='filter-scale-name'), 
+        ], id='number-title')
+    ]),
+    html.Div([
         html.Div([
-            html.H3(children='Total de vente', style={'margin': '0'}),
-            html.Label(id='nb-vente', style={'fontSize': '24px'})
-        ], style=cardStyle),
-
+            html.H3(children='Total de vente', className='cs-card-title'),
+            html.Label(id='nb-vente', className='cs-card-body')
+        ], className='cs-card'),
         html.Div([
-            html.H3(children='Moyenne des valeurs foncières', style={'margin': '0'}),
-            html.Label(id='avg-vente', style={'fontSize': '24px'})
-        ], style=cardStyle)
+            html.H3(children='Moyenne', className='cs-card-title'),
+            html.Label(id='avg-vente', className='cs-card-body')
+        ], className='cs-card'),
+        html.Div([
+            html.H3(children='Médiane', className='cs-card-title'),
+            html.Label(id='median-vente', className='cs-card-body')
+        ], className='cs-card'),
     ], style={'display': 'flex', 'margin': '32px 0', 'gap': '16px'}),
+    html.Div([
+        html.H3(children='Evolution des ventes en fonciton du temps', id='time-title')
+    ]),
     html.Div([
         html.Div([
             dcc.Graph(id='graph-content', style={'height':'400px'}),
-            dcc.Graph(id='graph-bar')
-        ], style={ 'width' : '60%', }),
+        ], className='col-md-8'),
         html.Div([
-            dcc.Graph(id='graph-map')
-        ], style={ 'width' : '40%', }),
-    ], style={ 'display' : 'flex', }),
+            dcc.Graph(id='years-graph', style={'height':'400px'}),
+        ], className='col-md-4'),
+    ], className='row', style={ 'display' : 'flex', }),
+    html.Div([
+        html.H3(children='Représentation géographique des ventes', id='geo-title')
+    ]),
     html.Div([
         html.Div([
-            dcc.Graph(id='pie-chart-local'),
-        ], style={ 'width' : '30%', }),
+            dcc.Graph(id='graph-map'),
+        ], className='col-md-8'),
         html.Div([
-            dcc.Graph(id='pie-chart-mutation')
-        ], style={ 'width' : '30%', }),
-    ], style={ 'display' : 'flex', })
+            
+        ], id='map-details', className='col-md-4'),
+    ], className='row'),
+    html.Div([
+        html.H3(children='Représentation des ventes par Type de bien', id='type-local-title')
+    ]),
+    html.Div([
+        html.Div([
+            dcc.Graph(id='graph-bar'),
+        ], className='col-md-6'),
+        html.Div([
+            dcc.Graph(id='pie-chart-local'),
+        ], className='col-md-6'),
+    ], className='row align-items-center'),
 ], className='mx-3')
 
 
 @callback(
-    Output('nb-vente', 'children'),
+    [
+        Output('nb-vente', 'children'),
+        Output('avg-vente', 'children'),
+        Output('median-vente', 'children'),
+        Output('filter-dates-name', 'children'),
+    ],
     Input('years-selection', 'value')
 )
 def update_stats(value):
     years =inputTolist(value)
     dff = df[df['annee'].isin(years)]
-    return dff.shape[0]
-
-
-@callback(
-    Output('avg-vente', 'children'),
-    Input('years-selection', 'value')
-)
-def update_stats(value):
-    years =inputTolist(value)
-    dff = df[df['annee'].isin(years)]
-    return round(dff['Valeur fonciere'].mean(), 2)
+    value = dff['Valeur fonciere']
+    if len(years) > 1:
+        dates = f'de {min(years)} à {max(years)}'
+    elif len(years) == 1:
+        dates = f'en {years[0]}'
+    else:
+        dates = 'par année'
+    return dff.shape[0], round(value.mean(), 2), round(value.median(), 2), dates
 
 
 @callback(
@@ -170,6 +145,24 @@ def update_graph(value):
         x='mois',
         y='Valeur fonciere',
         color='annee',
+        # title='Évolution de la moyenne de la valeur foncièce au cours de l\'année'
+    )
+    fig.update_layout(margin={'r': 0, 't': 24, 'l': 0, 'b': 0})
+    fig.update_layout(legend={'orientation': 'h', 'yanchor': 'top', 'y': 1.1, 'xanchor': 'right', 'x': 1 , 'bgcolor': 'rgba(255,0,0,0)'})
+    return fig
+
+@callback(
+    Output('years-graph', 'figure'),
+    Input('years-selection', 'value')
+)
+def update_years_graph(value):
+    years = inputTolist(value)
+    # dff = df[df['annee'].isin([2018, 2019, 2020, 2021])]
+    dff = df[df['annee'].isin(years)]
+    fig = px.bar(
+        dff.groupby(['annee'])['Valeur fonciere'].mean().reset_index(),
+        x='annee',
+        y='Valeur fonciere',
         # title='Évolution de la moyenne de la valeur foncièce au cours de l\'année'
     )
     fig.update_layout(margin={'r': 0, 't': 24, 'l': 0, 'b': 0})
@@ -199,7 +192,10 @@ def update_bar_graph(value):
 
 
 @callback(
-    Output('graph-map', 'figure'),
+    [
+        Output('graph-map', 'figure'),
+        Output('filter-scale-name', 'children'),
+    ],
     Input('scale-selection', 'value'),
     Input('years-selection', 'value')
 )
@@ -214,9 +210,30 @@ def update_map(value, year):
     fig.update_layout(showlegend=False,
                         margin={"r":0,"t":0,"l":0,"b":0},
                     )
-    return fig
+    return fig, scale
 
-
+@callback(
+    Output('map-details', 'children'),
+    [
+        Input('graph-map', 'clickData'),
+        Input('scale-selection', 'value'),
+        Input('years-selection', 'value')
+    ],
+        )
+def update_figure(clickData, scale, yValues):
+    print(type(clickData), clickData, scale)
+    if clickData is not None:
+        scale = echelle[scale]
+        years = inputTolist(yValues)
+        # recuperer le code departement ou le code region
+        location = clickData['points'][0]['location']
+        dff = df[df['annee'].isin(years)][df['code_departement'] == location]
+        print(dff.shape[0])
+        print(dff['Valeur fonciere'].mean())
+        print(dff.population.iloc[0])
+        # print(dff.loc[0, 'metre_carre']())
+        
+    return "blabla"
 @callback(
     Output('pie-chart-local', 'figure'),
     Input('years-selection', 'value')
@@ -225,19 +242,6 @@ def update_pie_local(value):
     years = inputTolist(value)
     dff = df[df['annee'].isin(years)].groupby(['annee', 'Type local'])['Valeur fonciere'].mean().reset_index()
     fig = px.pie(dff, values='Valeur fonciere', names='Type local', title='Proportion')
-    # fig.update_layout(legend={'orientation': 'h', 'yanchor': 'top', 'y': 1.02, 'xanchor': 'right', 'x': 1 , 'bgcolor': 'rgba(255,0,0,0)'})
-    fig.update_layout(showlegend=False)
-    return fig
-
-
-@callback(
-    Output('pie-chart-mutation', 'figure'),
-    Input('years-selection', 'value')
-)
-def update_pie_local(value):
-    years = inputTolist(value)
-    dff = df[df['annee'].isin(years)].groupby(['annee', 'Nature mutation'])['Valeur fonciere'].mean().reset_index()
-    fig = px.pie(dff, values='Valeur fonciere', names='Nature mutation', title='Proportion')
     # fig.update_layout(legend={'orientation': 'h', 'yanchor': 'top', 'y': 1.02, 'xanchor': 'right', 'x': 1 , 'bgcolor': 'rgba(255,0,0,0)'})
     fig.update_layout(showlegend=False)
     return fig
